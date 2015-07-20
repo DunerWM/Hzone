c48c998a660632e3aca46d9c925fb283c51ea150
summer- <hcjnweb@gmail.com>
2015-07-20 17:03:40 +0800

    前端结构修改

--- service/blogprovider.js
+++ service/blogprovider.js
@@ -4,30 +4,43 @@ var Server = require('mongodb').Server;
 var BSON = require('mongodb').BSON;
 var ObjectID = require('mongodb').ObjectID;
 
-BlogProvider = function(host, port) {
+BlogProvider = function (host, port) {
     this.db = new Db('zone', new Server(host, port, {safe: false}, {auto_reconnect: true}, {}));
-    this.db.open(function(){});
+    this.db.open(function () {
+    });
 }
 
-BlogProvider.prototype.getCollection = function(callback) {
-    this.db.collection("blog", function(error, blog_collection){
-        if(error) {
+BlogProvider.prototype.getCollectionBlog = function (callback) {
+    this.db.collection("blog", function (error, blog_collection) {
+        if (error) {
             callback(error);
-        }else{
+        } else {
             callback(null, blog_collection);
+            this.db.close();
         }
     })
 }
 
-BlogProvider.prototype.findAll = function(callback) {
-    this.getCollection(function(error, blog_collectoon){
-        if(error){
+BlogProvider.prototype.getCollectionUser = function (callback) {
+    this.db.collection('user', function (error, user_collection) {
+        if (error) {
             callback(error);
-        }else{
-            blog_collectoon.find().toArray(function(error, results) {
-                if(error) {
+        } else {
+            callback(null, user_collection);
+            this.db.close();
+        }
+    })
+}
+
+BlogProvider.prototype.findAll = function (callback) {
+    this.getCollectionBlog(function (error, blog_collectoon) {
+        if (error) {
+            callback(error);
+        } else {
+            blog_collectoon.find().toArray(function (error, results) {
+                if (error) {
                     callback(error);
-                }else{
+                } else {
                     callback(null, results);
                 }
             })
@@ -35,20 +48,60 @@ BlogProvider.prototype.findAll = function(callback) {
     })
 }
 
-BlogProvider.prototype.save = function(blogs, callback) {
-    this.getCollection(function(error, blog_colection) {
-        if(error){
+BlogProvider.prototype.save = function (blogs, callback) {
+    this.getCollectionBlog(function (error, blog_colection) {
+        if (error) {
             callback(error);
-        }else{
-            if(typeof(blogs.length) == "undefind") {
+        } else {
+            if (typeof(blogs.length) == "undefind") {
                 blogs = [blogs];
             }
-            for(var i = 0; i < blogs.length; i++) {
+            for (var i = 0; i < blogs.length; i++) {
                 blog = blogs[i];
                 blog.create_at = new Date();
             }
-            blog_colection.insert(blogs, function() {
+            blog_colection.insert(blogs, function () {
                 callback(null, blogs);
+            })
+        }
+    })
+}
+
+BlogProvider.prototype.findAllBlogs = function (callback) {
+    var db = this.db;
+    db.collection("blog", function (error, blog_collection) {
+        if (error) {
+            callback(error);
+        } else {
+            blog_collection.find().toArray(function (error, results) {
+                if (error) {
+                    callback(error);
+                } else {
+                    if (typeof(results.length) == "undefind") {
+                        results = [results];
+                    }
+                    var docs = [];
+                    for (var i = 0; i < results.length; i++) {
+                        (function(resultObj, i) {
+                            db.collection("user", function (error, user_collection) {
+                                if (error) {
+                                    callback(error);
+                                } else {
+                                    user_collection.find({'id': resultObj.userId}).toArray(function (error, doc) {
+                                        if (error) {
+                                            callback(error);
+                                        } else {
+                                            docs.push({"doc": doc, "blog": resultObj});
+                                            if(i == results.length-1) {
+                                                callback(null, docs);
+                                            }
+                                        }
+                                    });
+                                }
+                            })
+                        })(results[i], i)
+                    }
+                }
             })
         }
     })
